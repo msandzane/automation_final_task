cd8b870633c6d8aac6792250e34d8293fec41afe
msandzane <ieva.andzane@gmail.com>
2024-02-25 21:11:41 +0200

    first commit

---
+++ test-data/users.js
@@ -0,0 +1,8 @@
+export const users = {
+    Ieva: {
+        email: "ieva.andzane@gmail.com",
+        password: "ievalovescoding12345",
+        firstname: "Ieva",
+        lastname: "Andzane"
+    }
+};
