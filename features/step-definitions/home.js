8319b362e3ca5a56a550d8c01d91abf0888082cf
msandzane <160149672+msandzane@users.noreply.github.com>
2024-02-28 17:28:42 +0200

    Update home.js

--- features/step-definitions/home.js
+++ features/step-definitions/home.js
@@ -1,12 +1,17 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 import homePage from '../page-objects/home.page.js';
-
-
+import authenticationPage  from `../page-objects/authentication.page.js`;
 
 
 Given('I am on the home page', async function() {
     await homePage.open();
 });
 
+When(`I click on the "Best sellers" button`, async function() {
+    await homePage.bestSellersButton.click();
+});
 
-
+Then(`I see two forms displayed to Create an account or Sign in`, async function() {
+    await expect.toHaveText(this.signInOffer);
+    await expect.toHaveText(this.logInOffer);
+});
